4df120ba9b76411adabd86aff876c710c20ea09a
Marcin Pogorzelski <marcinpogorzelski@MacBook-Air-Marcin.local>
2026-02-06 21:34:07 +0100

    added page for planner

--- MealPlannerHTML/data.js
+++ MealPlannerHTML/data.js
@@ -3,81 +3,81 @@ const dania = {
   "śniadanie": [
     {
       nazwa: "Owsianka z bananem i orzechami",
-      kalorie: 350,
+      kalorie: 420,
       skladniki: {'Płatki owsiane': [40,70],'Mleko 2%': [120,200],'Banan': [60,100],'Orzechy włoskie':[5,10],'Izolat białka':[0,20]}
     },
     {
       nazwa: "Jajecznica z papryką",
-      kalorie: 320,
+      kalorie: 390,
       skladniki: {'Jajka': [2,3],'Papryka':[40,80],'Pieczywo pełnoziarniste':[40,80],'Masło':[5,10]}
     },
     {
       nazwa: "Musli z jogurtem greckim i jagodami",
-      kalorie: 340,
+      kalorie: 410,
       skladniki: {'Musli': [40,70],'Jogurt grecki': [100,200],'Maliny/jagody mrożone': [50,100]}
     },
     {
       nazwa: "Kanapka z kurczakiem",
-      kalorie: 310,
+      kalorie: 380,
       skladniki: {'Kurczak gotowany':[40,70],'Sałata':[20,40],'Pomidor':[40,80],'Pieczywo pełnoziarniste':[40,80]}
     },
     {
       nazwa: "Musli z jogurtem i jabłkiem",
-      kalorie: 330,
+      kalorie: 400,
       skladniki: {'Musli': [40,70],'Jogurt naturalny': [100,200],'Jabłko/gruszka': [60,120]}
     },
     {
       nazwa: "Jajecznica ze szczypiorkiem",
-      kalorie: 320,
+      kalorie: 390,
       skladniki: {'Jajka': [2,3],'Szczypiorek':[5,10],'Pieczywo pełnoziarniste':[40,80],'Masło':[5,10]}
     },
     {
       nazwa: "Kasza jaglana z jogurtem i owocami",
-      kalorie: 360,
+      kalorie: 430,
       skladniki: {'Kasza jaglana': [40,70],'Jogurt naturalny': [100,200],'Jabłko/gruszka': [50,100],'Orzechy włoskie':[5,10]}
     }
   ],
   "obiad": [
     {
       nazwa: "Kurczak pieczony z batatami",
-      kalorie: 480,
+      kalorie: 580,
       skladniki: {'Pierś kurczaka':[120,170],'Bataty':[100,200],'Warzywa (mix)':[100,200],'Oliwa':[5,10]}
     },
     {
       nazwa: "Indyk z kaszą bulgur",
-      kalorie: 460,
+      kalorie: 560,
       skladniki: {'Filet z indyka':[120,170],'Kasza bulgur':[40,80],'Marchew':[40,70],'Brokuł':[60,100],'Oliwa':[5,10]}
     },
     {
       nazwa: "Kurczak z makaronem w passacie",
-      kalorie: 470,
+      kalorie: 570,
       skladniki: {'Pierś kurczaka':[120,170],'Makaron pełnoziarnisty':[40,80],'Cukinia':[40,80],'Papryka':[40,80],'Passata pomidorowa':[100,150]}
     },
     {
       nazwa: "Kurczak z kaszą jaglaną i marchewką",
-      kalorie: 450,
+      kalorie: 550,
       skladniki: {'Pierś kurczaka':[120,170],'Kasza jaglana':[40,80],'Marchew':[40,80],'Passata pomidorowa':[80,120]}
     },
     {
       nazwa: "Kurczak z fasolką szparagową",
-      kalorie: 440,
+      kalorie: 540,
       skladniki: {'Pierś kurczaka':[120,170],'Fasolka szparagowa':[80,130],'Ryż':[40,80],'Olej rzepakowy':[5,10]}
     },
     {
       nazwa: "Indyk pieczony z warzywami",
-      kalorie: 465,
+      kalorie: 565,
       skladniki: {'Indyk (pieczony filet)':[120,170],'Marchew':[40,80],'Cukinia/papryka':[40,80],'Ziemniaki':[80,150],'Oliwa':[5,10]}
     },
     {
       nazwa: "Pulpeciki z indyka z ryżem",
-      kalorie: 455,
+      kalorie: 555,
       skladniki: {'Kulki/pulpeciki z indyka':[120,170],'Ryż':[40,80],'Warzywa do pieczenia (cukinia, marchew)':[80,150],'Oliwa':[5,10]}
     }
   ],
   "kolacja": [
     {
       nazwa: "Sałatka z tuńczykiem",
-      kalorie: 290,
+      kalorie: 350,
       skladniki: {'Tuńczyk w wodzie':[50,100],'Kukurydza':[25,50],'Papryka/ogórek':[50,100],'Sałata':[30,50],'Pieczywo pełnoziarniste':[40,80]}
     },
     {
@@ -102,29 +102,29 @@ const dania = {
     },
     {
       nazwa: "Wędzony łosoś z sałatką",
-      kalorie: 270,
+      kalorie: 330,
       skladniki: {'Wędzony łosoś':[40,80],'Sałata':[30,50],'Kukurydza':[25,50],'Pieczywo pełnoziarniste':[40,80]}
     },
     {
       nazwa: "Hummus z warzywami",
-      kalorie: 250,
+      kalorie: 310,
       skladniki: {'Hummus':[40,80],'Pomidor':[40,80],'Pieczywo chrupkie':[20,40]}
     }
   ],
   "podwieczorek": [
     {
       nazwa: "Owoce z orzechami",
-      kalorie: 150,
+      kalorie: 180,
       skladniki: {'Jabłko/gruszka': [80,120],'Orzechy włoskie':[10,20]}
     },
     {
       nazwa: "Jogurt z owocami",
-      kalorie: 120,
+      kalorie: 150,
       skladniki: {'Jogurt naturalny': [100,150],'Maliny/jagody mrożone':[50,80]}
     },
     {
       nazwa: "Batony proteinowe",
-      kalorie: 180,
+      kalorie: 200,
       skladniki: {'Baton proteinowy': [1,1]}
     }
   ]
